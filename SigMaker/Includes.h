80df891be94bb2ded64bc2c45b4ccc8eb65298a0
gir489 <poop@butt.com>
2018-06-14 21:33:51 -0500

    Removed extraneous includes from Includes.h.

--- SigMaker/Includes.h
+++ SigMaker/Includes.h
@@ -8,26 +8,9 @@
 #pragma warning( push )  
 #pragma warning( disable : 4267 )
 #pragma warning( disable : 4244 )
-#include <ida.hpp>
-#include <idp.hpp>
-#include <enum.hpp>
-#include <frame.hpp> 
 #include <expr.hpp>
-#include <name.hpp>
-#include <segment.hpp>
-#include <bytes.hpp>
-#include <struct.hpp>
 #include <loader.hpp>
-#include <kernwin.hpp>
-#include <auto.hpp>
-#include <entry.hpp>
-#include <bytes.hpp>
-#include <typeinf.hpp>
-#include <demangle.hpp>
-#include <allins.hpp>
 #include <search.hpp> // find_binary
-#include <ua.hpp>
-#include <fpro.h>
 #include <diskio.hpp>
 #pragma warning( pop ) 
 
